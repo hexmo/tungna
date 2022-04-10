dd681b0c124de3cd34019de660bfac290de6a4bd
mohitacharya <mohitacharya@bajratechnologies.com>
2022-04-10 22:23:51 +0545

    feat: product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
   has_many_attached :images
+
+  validates :name, :description, :price, :images, :category, :brand, presence: true
+
+  enum category: {
+    guitar: 'Guitar',
+    drum: 'Drum',
+    piano: 'Piano & Keyboard',
+    ukulele: 'Ukulele',
+    wind_pipes: 'Wind pipes',
+    traditional: 'Traditional Instruments',
+    home_studio: 'Home Studio'
+  }
 end
